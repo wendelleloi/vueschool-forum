9aad84ad926cf45a7a3deb54c70de27ce65de795
wendelleloi <wendelleloi2@gmail.com>
2020-04-05 14:57:32 -0300

    :lipstick: Adiciona estilo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,34 @@
 <template>
-  <div>
-    <h1>
-      Welcome to the forum
-    </h1>
-    <div v-for="(thread, index) in threads" :key="index">
-      <h2> {{ thread.title }} </h2>
-      <div v-for="(postId, index) in thread.posts" :key="index">
-        <p> {{ users[posts[postId].userId].name }} </p>
-        <p> {{ posts[postId].text }} </p>
+<div>
+  <div v-for="(thread, index) in threads" :key="index" class="col-large push-top">
+    <h1>{{ thread.title }}</h1>
+    <div class="post-list">
+      <div class="post" v-for="(postId, index) in thread.posts" :key="index">
+        <div class="user-info">
+          <a href="#" class="user-name"> {{ users[posts[postId].userId].name }} </a>
+
+          <a href="#">
+              <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="">
+          </a>
+
+          <p class="desktop-only text-small">107 posts</p>
+
+        </div>
+
+        <div class="post-content">
+            <div>
+              {{ posts[postId].text }}
+            </div>
+        </div>
+
+        <div class="post-date text-faded">
+            {{ posts[postId].publishedAt }}
+        </div>
+
       </div>
     </div>
   </div>
+</div>
 </template>
 
 <script>
