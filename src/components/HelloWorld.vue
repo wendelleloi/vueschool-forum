3fba1c6cfcb42096864bfe53d45c3524745aeede
wendelleloi <wendelleloi2@gmail.com>
2020-04-06 18:33:57 -0300

    :art: lista as threads em componentes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,7 @@
 <template>
-<div>
-  <div v-for="(thread, index) in threads" :key="index" class="col-large push-top">
-    <h1>{{ thread.title }}</h1>
-    <div class="post-list">
-      <div class="post" v-for="(postId, index) in thread.posts" :key="index">
-        <div class="user-info">
-          <a href="#" class="user-name"> {{ users[posts[postId].userId].name }} </a>
-
-          <a href="#">
-              <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="">
-          </a>
-
-          <p class="desktop-only text-small">107 posts</p>
-
-        </div>
-
-        <div class="post-content">
-            <div>
-              {{ posts[postId].text }}
-            </div>
-        </div>
-
-        <div class="post-date text-faded">
-            {{ posts[postId].publishedAt }}
-        </div>
-
-      </div>
-    </div>
-  </div>
+<div class="col-full">
+  <h1>Welcome to the forum</h1>
+  <ThreadList :threads="threads"/>
 </div>
 </template>
 
@@ -37,10 +11,13 @@ console.log(sourceData)
 export default {
   data () {
     return {
-      threads: sourceData.threads,
+      threads: Object.values(sourceData.threads),
       posts: sourceData.posts,
       users: sourceData.users
     }
+  },
+  components: {
+    ThreadList: () => import('./ThreadList')
   }
 }
 </script>
---
+++ src/components/ThreadList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="thread-list">
+    <h2 class="list-title">Threads</h2>
+    <ThreadListItem
+      v-for="thread in threads"
+      :key="thread['.key']"
+      :thread="thread"
+    />
+  </div>
+</template>
+
+<script>
+// import ThreadListItem from './ThreadListItem'
+export default {
+  components: {
+    ThreadListItem: () => import('./ThreadListItem')
+  },
+  props: {
+    threads: {
+      required: true,
+      type: Array
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
